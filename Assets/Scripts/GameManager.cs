a282eb6a1a295119c96120c5fca71fa88cdb0ba1
Vulkan1k <nandi.castlegarden@gmail.com>
2021-11-01 21:04:59 +0700

    QUiz debug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
+using NaughtyAttributes;
 
 public class GameManager : MonoBehaviour {
 
@@ -17,8 +18,46 @@ public class GameManager : MonoBehaviour {
 
 	public Canvas endGameCanvas;
 
+	[HorizontalLine(color: EColor.Gray)]
+	public bool isQuiz;
+	public int currentQuizPage;
+
+	[Serializable]
+	public class QuestionAnswer
+	{
+		public string question;
+		public int rightAnswer;
+	}
+
+	public List<QuestionAnswer> questionAnswersBank;
+	public List<QuestionAnswer> questionAnswers;
+
+	[HorizontalLine(color: EColor.Gray)]
+	[ReadOnly]
+	public string result;
+
+	[Button(enabledMode: EButtonEnableMode.Playmode)]
+	void AnswerYes() 
+	{
+		int rightAnswer = questionAnswers[currentQuizPage].rightAnswer;
+
+		result = rightAnswer == 0 ? "You're right!" : "You're wrong!";
+		NextQuizPage();
+	}
+
+	[Button(enabledMode: EButtonEnableMode.Playmode)]
+	void AnswerNo() 
+	{
+		int rightAnswer = questionAnswers[currentQuizPage].rightAnswer;
+
+		result = rightAnswer == 0 ? "You're wrong!" : "You're right!";
+		NextQuizPage();
+	}
+
 	void Start () {
 		timer_current = timer;
+
+		PrepareQuestionAnswers();
 	}
 	
 	void Update () {
@@ -43,5 +82,23 @@ public class GameManager : MonoBehaviour {
 
 	void UpdateUI () {
 		timerText.text = (int)timer_current + "s";
+	}
+	
+	void PrepareQuestionAnswers()
+	{
+		while(questionAnswers.Count < 5)
+		{
+			int r = Random.Range(0, questionAnswersBank.Count);
+			questionAnswers.Add(questionAnswersBank[r]);
+			questionAnswersBank.RemoveAt(r);
+		}
+	}
+
+	void NextQuizPage()
+	{
+		if(currentQuizPage < 5 - 1)
+		{
+			currentQuizPage++;
+		}
 	}
 }
